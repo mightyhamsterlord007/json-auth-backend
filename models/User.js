702b41c4f2f358d74144a8dea03b7ae93da902bd
Pak <Pak@admins-iMac-69.local>
2018-09-18 12:08:20 -0400

    add model

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new mongoose.Schema({
+    email: {type: String, default: ''},
+    username: {type: String, default: ''},
+    password: {type: String, default: ''},
+    timestamp: {type: String, default: now.format('dddd, MMMM Do YYYY, h:mm:ss a')}
+});
